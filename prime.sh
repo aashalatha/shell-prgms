4d15e246f5c9e222b0ad46464f286774ad129192
Aashalatha P A <93538340+aashalatha@users.noreply.github.com>
2022-07-19 17:55:42 +0530

    Create prime.sh

---
+++ prime.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "Enter a number:"
+read num
+for((i=2; i<=$num/2; i++))
+do
+	ans=$(( num%i ))
+	if [ $ans -eq 0 ]
+	then 
+		echo "$num is not a prime number"
+		exit 0
+	fi
+done
+echo "$num is a prime number"
